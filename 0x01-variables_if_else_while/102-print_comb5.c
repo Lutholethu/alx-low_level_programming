52a52f50fe10ef87e75ef8c225665e27e0d4cc95
Elethu <filtaneelethu@gmail.com>
2023-09-11 16:52:36 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - main function to print two digit number combinations, without any repetition
+ * main - main function to print two digit number combinations
+ * without any repetion
  *
  * Return: always 0
  */
@@ -11,18 +12,18 @@ int main(void)
 	int i = '0';
 	int j = '0';
 
-	while (i <= 99)
+	for  (i = 0 ; i <= 99)
 	{
-		while (j <= 99)
+		while (j = 0 ; j <= 99)
 		{
 			if (i < j && i != j)
 			{
-				putchar((i/10) + '0');
-				putchar((i%10) + '0');
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((j/10) + '0');
-				putchar((j%10) + '0');
-				if ( i != 98 || j != 99)
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
